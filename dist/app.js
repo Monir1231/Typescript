5006184de06a2bd562b528d4b1f32ad74dee9a65
Monir1231 <monirmolla324@gmail.com>
2026-01-16 21:11:02 +0600

    typescript

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,58 @@
 "use strict";
 // string 
 Object.defineProperty(exports, "__esModule", { value: true });
-let name = "monir";
-console.log(name);
+// let name:string = "monir"
+// console.log(name)
 // array
-let number = [12, 34,];
-console.log(number);
-let number2 = [12, 34,];
-console.log(number2);
-// tupple 
-const mytup = ["monir", "rana", 12, true];
-console.log(mytup);
+// let number:number[] = [12,34,]
+// console.log(number)
+// let number2:Array<number> = [12,34,]
+// console.log(number2)
+// tupple
+// const mytup:[string,string,number,boolean] = ["monir","rana",12,true]
+// console.log(mytup)
+// any, unknown, void, null, undefined
+// any
+let random = "monir";
+random = 23;
+random = true;
+console.log(random);
+// unknown
+let nam = "monir";
+if (typeof nam === "string") {
+    console.log(nam.toUpperCase());
+}
+// func  void
+function usernum() {
+    console.log("this is new bangladesh");
+}
+usernum();
+function info(name, age) {
+    return `my name is ${name} and age ${age}`;
+}
+console.log(info("monir", 23));
+// undifine
+let number;
+console.log(typeof number);
+const student1 = {
+    name: "monir",
+    age: 23,
+    course: "font-end devoloper",
+    skills: ["js", "react", "next",],
+    info: ["monir", 34, true, null]
+};
+const student2 = {
+    name: "monir",
+    age: 27,
+    course: "font-end devoloper",
+    skills: ["js", "react", "next",],
+    info: ["monir", 34, true, null]
+};
+console.log(student1, student2);
+function data(student) {
+    console.log(`Name:${student.name}`);
+    console.log(`Age:${student.age}`);
+    console.log(`Course:${student.course}`);
+}
+data(student1);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,103 @@
 
 // string 
 
-let name:string = "monir"
-console.log(name)
+// let name:string = "monir"
+// console.log(name)
+
+
 
 // array
-let number:number[] = [12,34,]
-console.log(number)
+// let number:number[] = [12,34,]
+// console.log(number)
 
-let number2:Array<number> = [12,34,]
-console.log(number2)
 
-// tupple 
 
-const mytup:[string,string,number,boolean] = ["monir","rana",12,true]
-console.log(mytup)
+// let number2:Array<number> = [12,34,]
+// console.log(number2)
+
+
+
+// tupple
+// const mytup:[string,string,number,boolean] = ["monir","rana",12,true]
+// console.log(mytup)
+
+
+
+// any, unknown, void, null, undefined
+
+
+
+// any
+let random:any = "monir"
+random = 23
+random = true
+
+console.log(random)
+
+
+// unknown
+let nam:unknown = "monir"
+
+if(typeof  nam === "string"){
+    console.log(nam.toUpperCase())
+}
+
+// func  void
+
+function usernum(): void {
+    console.log("this is new bangladesh")
+}
+usernum()
+
+function info (name:string,age:number):string | number{
+  return `my name is ${name} and age ${age}`
+}
+console.log(info("monir",23))
+
+
+// undifine
+
+let number:undefined
+console.log(typeof number)
+
+// null
+// let number2:null = null
+// console.log(number2)
+
+// student profile
+
+type student = {
+    name:string,
+    age:number,
+    course:string,
+    skills:string[],
+    info:[string,number,boolean,null]
+
+}
+
+const student1:student ={
+    name:"monir",
+    age:23,
+    course:"font-end devoloper",
+    skills:["js","react","next",],
+    info:["monir",34,true,null]
+}
+
+
+const student2:student ={
+    name:"monir",
+    age:27,
+    course:"font-end devoloper",
+    skills:["js","react","next",],
+    info:["monir",34,true,null]
+}
+
+console.log(student1,student2)
+
+
+function data (student:student):void{
+    console.log(`Name:${student.name}`)
+     console.log(`Age:${student.age}`)
+       console.log(`Course:${student.course}`)
+}
+data(student1)
