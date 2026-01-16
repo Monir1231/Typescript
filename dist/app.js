a8218b754fbc67b8dd38ff47496fab9f5de0bde6
Monir1231 <monirmolla324@gmail.com>
2026-01-16 10:39:21 +0600

    typescript

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+"use strict";
+// string 
+Object.defineProperty(exports, "__esModule", { value: true });
+let name = "monir";
+console.log(name);
+// array
+let number = [12, 34,];
+console.log(number);
+let number2 = [12, 34,];
+console.log(number2);
+// tupple 
+const mytup = ["monir", "rana", 12, true];
+console.log(mytup);
+//# sourceMappingURL=app.js.map
---
+++ src/app.ts
@@ -0,0 +1,18 @@
+
+
+// string 
+
+let name:string = "monir"
+console.log(name)
+
+// array
+let number:number[] = [12,34,]
+console.log(number)
+
+let number2:Array<number> = [12,34,]
+console.log(number2)
+
+// tupple 
+
+const mytup:[string,string,number,boolean] = ["monir","rana",12,true]
+console.log(mytup)
